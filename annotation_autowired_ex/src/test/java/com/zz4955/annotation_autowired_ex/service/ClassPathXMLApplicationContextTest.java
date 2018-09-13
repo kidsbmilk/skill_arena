5934c0c76302f85ae016b8c01a78e906f4761216
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-09-13 14:42:25 +0800

    add urls

--- annotation_autowired_ex/src/test/java/com/zz4955/annotation_autowired_ex/service/ClassPathXMLApplicationContextTest.java
+++ annotation_autowired_ex/src/test/java/com/zz4955/annotation_autowired_ex/service/ClassPathXMLApplicationContextTest.java
@@ -1,5 +1,9 @@
 package com.zz4955.annotation_autowired_ex.service;
 
+/**
+ * Spring注解的原理与自定义注解的实现：https://blog.csdn.net/Wooyulin/article/details/79158483
+ * 以@Resource为例，透析注解的本质，spring中是如何使用注解的：https://blog.csdn.net/qq_27093465/article/details/52702463
+ */
 public class ClassPathXMLApplicationContextTest {
 
     public static void main(String[] args) {
