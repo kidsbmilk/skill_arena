49c0fb0802b94619f9b53ddc3d9b00a4de4d682c
soybeanmilk <1101574955@qq.com>
2019-11-01 13:15:37 +0800

    add @Slf4j

--- log4j2filterlogid/src/main/java/com/zz4955/log4j2filterlogid/filter/MyWebFilter.java
+++ log4j2filterlogid/src/main/java/com/zz4955/log4j2filterlogid/filter/MyWebFilter.java
@@ -1,5 +1,6 @@
 package com.zz4955.log4j2filterlogid.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
+@Slf4j
 @Configuration
 @WebFilter(filterName = "MyWebFilter", urlPatterns = "/*")
 public class MyWebFilter implements Filter {
